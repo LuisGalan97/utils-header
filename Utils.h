cf6d56b24393077fcaf65019f2697c603b83e92e
Luis Galan <miguelgalan97@gmail.com>
2025-06-17 13:47:24 -0500

    modified:   Utils.h

--- Utils.h
+++ Utils.h
@@ -51,7 +51,7 @@ namespace utils{
     typename std::enable_if<!std::is_class<T>::value, void>::type
     printMemoryImp(const char* name, T& valor){
         std::cout << "------------------- Print Memory --------------------\n";
-        sstd::cout << "Input(#): \"" << name << "\"\n";
+        std::cout << "Input(#): \"" << name << "\"\n";
         std::cout << "[lvalue - Variable/Referencia/Instruccion]\nReserva " << sizeof(valor) << " bytes de memoria\n\n";
         showType(valor);
         std::cout << "   Valor: ";
